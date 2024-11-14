d229ef93fa6fd74a3077c41f718cb6f741c8033c
Ruth <ruthasafuadjaye@yahoo.com>
2024-11-14 16:34:44 +0200

    Remove Portfolio file and file in main index.html file

--- Portfolio 4/file.js
+++
@@ -1,6 +0,0 @@
-const goToAbout = document.querySelector('.arrow');
-
-goToAbout.addEventListener('click',() => {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 1500;
-})
